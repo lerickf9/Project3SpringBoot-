9eebebebc2c987b1b5905d255fc918314af2fa25
Erick <efuentes.edf9@gmail.com>
2024-06-10 21:33:43 -0400

    3er springboot with library chat GPT

--- src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -6,7 +6,7 @@ import com.theokanning.openai.service.OpenAiService;
 public class ConsultaChatGPT {
 
     public static String obtenerTraduccion(String texto){
-        OpenAiService service = new OpenAiService("sk-proj-4jenxWa5136TtcBQf3nRT3BlbkFJFmzxxHn9RvisYErpCnW0");
+        OpenAiService service = new OpenAiService("");
 
         CompletionRequest requisicion = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
